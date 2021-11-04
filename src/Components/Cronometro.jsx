b6ae5ba0386724e7869eb1797c10e89086859bc2
Jhonny Mendoza <jhonnymendoza@Mac-mini-de-Jhonny.local>
2021-11-03 22:07:42 -0300

    se modifica estilos del display

--- src/Components/Cronometro.jsx
+++ src/Components/Cronometro.jsx
@@ -5,9 +5,9 @@ export const Cronometro = (props) => {
     
     return (
         <div className="display">
-            <h2>{(props.tiempo.h >=10)?props.tiempo.h :"0"+props.tiempo.h}&nbsp;:</h2>
-            <h2>{(props.tiempo.m >=10)?props.tiempo.m :"0"+props.tiempo.m}&nbsp;:</h2>
-            <h2>{(props.tiempo.s >=10)?props.tiempo.s :"0"+props.tiempo.s}&nbsp;:</h2>
+            <h2>{(props.tiempo.h >=10)?props.tiempo.h :"0"+props.tiempo.h}</h2><span>:</span>
+            <h2>{(props.tiempo.m >=10)?props.tiempo.m :"0"+props.tiempo.m}</h2><span>:</span>
+            <h2>{(props.tiempo.s >=10)?props.tiempo.s :"0"+props.tiempo.s}</h2><span>:</span>
             <h2>{(props.tiempo.ms >=10)?props.tiempo.ms :"0"+props.tiempo.ms}</h2>
         </div>
     )
